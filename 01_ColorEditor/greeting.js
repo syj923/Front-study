8d8a2ba413bb270870eb0f87392407e6203c8f25
유진공주 <yujinprincess@yujingongjuui-MacBookAir.local>
2021-04-03 17:50:04 +0900

    first commit

---
+++ 01_ColorEditor/greeting.js
@@ -0,0 +1,19 @@
+const form = document.querySelector(".js-form"),
+    input = document.querySelector("input");
+
+const USER_LS = "currentUser"
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null){
+
+    } else {
+        
+    }
+}
+
+function init(){
+
+}
+
+init();
